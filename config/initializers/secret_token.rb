b919c47413f0c6b71de9fb97a649c571ce206fa5
Brett <brett@socketstudio.com>
2012-08-17 08:49:07 -0400

    first structure in place

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PopvidIo::Application.config.secret_token = 'b2f50edeb65c6fdd9c798b2e6f9edc2437b233c366e8d41aedd0edbe42d58e2b5d7eacbd02545d305186cd395a50d6bc26a0465a04e52e3313398f97ae48a58a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PopvidIo::Application.config.session_store :cookie_store, key: '_popvid.io_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PopvidIo::Application.config.session_store :active_record_store
